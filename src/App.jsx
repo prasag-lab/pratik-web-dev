fd27d369493388437ca2362c03ff2b4bbed9a000
prasag-lab <sagarsamir676@gmail.com>
2026-02-22 13:12:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='lol'>
+
+      <h1>hi this is pratik</h1>
+      </div>
+      
+    
+    </>
+  )
+}
+
+export default App
